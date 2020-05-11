56b8ad9dcea88000fc3604f66aa7c0a61f45564d
Eminerve <60135711+Eminerve@users.noreply.github.com>
2020-05-11 17:25:06 +0200

    comments daily

--- daily.php
+++ daily.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Spray-it Daily</title>
   </head>
   <body>
+
+    <!-- Inclusion de la navigation -->
     <?php include("nav.php"); ?>
 
+    <!-- section à copie pour ajouter un nouveau daily / balise figure à copier pour ajouter une photo au groupe-->
     <!-- <section class="principal">
       <article class="photo">
         <figure>
@@ -29,7 +32,6 @@
 
     <section class="principal">
       <article class="photo">
-
         <figure>
           <img src="images/daily/2020/Holem-Covid-19-City-web.jpg" alt="daily spray it">
           <figcaption></figcaption>
@@ -103,11 +105,12 @@
       </aside>
     </section>
 
-
-
+    <!-- inclusion du pied de pages -->
     <?php include("footer.php"); ?>
 
     <style>
+
+    /* ---------------- Fonts de la page --------------- */
 
     @font-face {
       font-family: 'OpenSans';
@@ -133,6 +136,8 @@
       src: url('fonts/open-sans-semibold.ttf');
     }
 
+    /* --- Caractéristiques générales de la page ----- */
+
     body {
     animation: fadeIn ease 3s;
     -webkit-animation: fadeIn ease 3s;
@@ -141,7 +146,7 @@
     -ms-animation: fadeIn ease 3s;
     }
 
-
+    /* Apparition progressive Body */
     @keyframes fadeIn{
     0% {
       opacity:0;
@@ -187,54 +192,59 @@
     }
     }
 
+    /* ---------------- Section Principale --------------- */
 
-      .principal{
+    .principal{
         font-family: 'OpenSans';
         display: flex;
         padding-top: 8rem;
         width: 78%;
         margin: auto;
         align-items: flex-start;
-      }
+    }
 
-      figure{
+    /* Partie Photo */
+
+    figure{
         width: 143%;
         padding-bottom: 4rem;
-      }
+    }
 
-      figure img{
+    figure img{
         width: 100%;
-      }
+    }
 
-      .laius {
+    figcaption {
+        font-size: 12px;
+        font-family: 'OpenSans';
+        color: #777;
+        float: right;
+    }
+
+    /* Partie Texte */
+
+    .laius {
         width: 90%;
         margin-left: 3%;
         padding-left: 30%;
-      }
+    }
 
-      .laius h1 {
+    .laius h1 {
         width: 18rem;
         font-size: 29px;
         font-weight: 600;
         text-align: left;
         color: #303133;
-      }
+    }
 
-      .laius p {
+    .laius p {
         width: 18rem;
         font-size: 13px;
         font-weight: 300;
         text-align: justify;
         margin-top: 15px;
         color: #777;
-      }
-
-      figcaption {
-        font-size: 12px;
-        font-family: 'OpenSans';
-        color: #777;
-        float: right;
-      }
+    }
 
     </style>
 
