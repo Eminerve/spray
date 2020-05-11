f7c0f88d84525c1eb98ee31858eecddd2456cdaf
Eminerve <60135711+Eminerve@users.noreply.github.com>
2020-05-11 17:16:03 +0200

    comments contact

--- contact.php
+++ contact.php
@@ -6,198 +6,220 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Spray-it Contact</title>
 </head>
-<body>
-  <?php include('nav.php'); ?>
-  <section id="contact">
-      <div class="informations">
+  <body>
+
+    <!-- Iclusion de la page nav.php -->
+    <?php include('nav.php'); ?>
+
+    <!-- section principale de la page -->
+    <section id="contact">
+
+        <!-- Lien d'information et des médias -->
+        <div class="informations">
+            <p>
+              <h1>Informations</h1><br>
+              <h2> <a href="https://www.google.com/search?q=BORGARGATAN+2%2C+117+34+Stockholm&oq=BORGARGATAN+2%2C+117+34+Stockholm&aqs=chrome..69i57.3980j0j7&sourceid=chrome&ie=UTF-8" target="_blank">Borgargatan 2,<br>117 34 Stockholm</a></h2><br>
+              <h2> <a href="mailto:sprayit.collective@gmail.com" target="_blank">sprayit.collective@gmail.com</a></h2><br>
+              <h2>+46 (0) 73 617 95 11</h2>
+            </p><br><br>
+            <p>
+                <h1>Medias</h1><br>
+                <div class="media">
+                  <h2> <a href="https://www.facebook.com/sprayit.collective/" target="_blank"> <img src="images/medias/fb_nr.png" alt="facebook sprayit"></h2></a>
+                  <h2><a href="https://www.instagram.com/sprayit.collective/" target="_blank"> <img src="images/medias/ig_nr.png" alt="instagram sprayit"></h2></a>
+                </div>
+            </p>
+        </div>
+
+        <!-- Partie téléchargement du portfolio -->
+        <div class="download">
           <p>
-            <h1>Informations</h1><br>
-            <h2> <a href="https://www.google.com/search?q=BORGARGATAN+2%2C+117+34+Stockholm&oq=BORGARGATAN+2%2C+117+34+Stockholm&aqs=chrome..69i57.3980j0j7&sourceid=chrome&ie=UTF-8" target="_blank">Borgargatan 2,<br>117 34 Stockholm</a></h2><br>
-            <h2> <a href="mailto:sprayit.collective@gmail.com" target="_blank">sprayit.collective@gmail.com</a></h2><br>
-            <h2>+46 (0) 73 617 95 11</h2>
+            <!-- Lien à changer selon l'adresse du portfolio -->
+            <a href="images/portfolio.zip" download>
+              <img src="images/medias/logo _DL.png" alt="Download Press Kit">
+            </a><br>
+            <h3>Download Spray-it's Portfolio</h3>
           </p><br><br>
-          <p>
-              <h1>Medias</h1><br>
-              <div class="media">
-                <h2> <a href="https://www.facebook.com/sprayit.collective/" target="_blank"> <img src="images/medias/fb_nr.png" alt="facebook sprayit"></h2></a>
-                <h2><a href="https://www.instagram.com/sprayit.collective/" target="_blank"> <img src="images/medias/ig_nr.png" alt="instagram sprayit"></h2></a>
-              </div>
-          </p>
-      </div>
-      <div class="download">
-        <p>
-          <a href="images/portfolio.zip" download>
-            <img src="images/medias/logo _DL.png" alt="Download Press Kit">
-          </a><br>
-          <h3>Download Spray-it's Portfolio</h3>
-        </p><br><br>
-      </div>
-  </section>
-  <?php include('footer.php'); ?>
-  <style>
+        </div>
 
-  @font-face {
-    font-family: 'OpenSans';
-    src: url('fonts/OpenSans-Light.ttf');
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-  }
+    </section>
 
-  @font-face {
-    font-family: 'OpenSans';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url('fonts/OpenSans-Regular.ttf');
-  }
-
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  html{
-    font-size: 14px;
-    font-family: 'OpenSans', halvetica, sans-serif,serif;
-    color: #303133;
-  }
-
-  body {
-  animation: fadeIn ease 3s;
-  -webkit-animation: fadeIn ease 3s;
-  -moz-animation: fadeIn ease 3s;
-  -o-animation: fadeIn ease 3s;
-  -ms-animation: fadeIn ease 3s;
-  }
+    <!-- Inclusion du pied de page -->
+    <?php include('footer.php'); ?>
 
 
-  @keyframes fadeIn{
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-  }
+    <style>
 
-  @-moz-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-  }
+    /* ---------------- Fonts de la page --------------- */
 
-  @-webkit-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-  }
+    @font-face {
+      font-family: 'OpenSans';
+      src: url('fonts/OpenSans-Light.ttf');
+      font-style: normal;
+      font-weight: 300;
+      font-display: swap;
+    }
 
-  @-o-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-  }
+    @font-face {
+      font-family: 'OpenSans';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: url('fonts/OpenSans-Regular.ttf');
+    }
 
-  @-ms-keyframes fadeIn {
-  0% {
-    opacity:0;
-  }
-  100% {
-    opacity:1;
-  }
-  }
+    /* --- Caractéristiques générales de la page ----- */
 
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
 
-  #contact{
-    padding-top: 8rem;
-    margin: auto;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    width: 60%;
-  }
+    html{
+      font-size: 14px;
+      font-family: 'OpenSans', halvetica, sans-serif,serif;
+      color: #303133;
+    }
 
-  .informations {
-    min-width: 320px;
-    color: #303133;
-    border-right: solid 1px #333;
-    margin-right: 20%;
-  }
+    body {
+    animation: fadeIn ease 3s;
+    -webkit-animation: fadeIn ease 3s;
+    -moz-animation: fadeIn ease 3s;
+    -o-animation: fadeIn ease 3s;
+    -ms-animation: fadeIn ease 3s;
+    }
 
-  .informations h1 {
-    color: #000;
-    font-size: 26px;
-    font-weight: 400;
-  }
+    /* Apparition progressive Body */
 
-  .informations p {
+    @keyframes fadeIn{
+    0% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+    }
 
-    color: #303133;
-    font-size: 16px;
-    font-weight: 300;
-  }
+    @-moz-keyframes fadeIn {
+    0% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+    }
 
-  .informations h2 {
+    @-webkit-keyframes fadeIn {
+    0% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+    }
 
-    color: #303133;
-    font-size: 16px;
-    font-weight: 300;
-  }
+    @-o-keyframes fadeIn {
+    0% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+    }
 
-  .informations a {
-    color: #303133;
-    text-decoration: none;
-    font-size: 16px;
-    font-weight: 300;
-  }
+    @-ms-keyframes fadeIn {
+    0% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+    }
 
-  .informations img{
-    width: 25px;
-  }
+    /* ---------------- Section Principale --------------- */
+    #contact{
+      padding-top: 8rem;
+      margin: auto;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      align-content: center;
+      width: 60%;
+    }
 
-  .media {
-    display: flex;
-  }
+    /* ---- Informations et Médias ------ */
+    .informations {
+      min-width: 320px;
+      color: #303133;
+      border-right: solid 1px #333;
+      margin-right: 20%;
+    }
 
-  .media a{
-    padding-right:7px;
-    padding-left: 7px;
-  }
+    .informations h1 {
+      color: #000;
+      font-size: 26px;
+      font-weight: 400;
+    }
 
-  .download {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    text-align: center;
-    color: #777;
-    font-weight: 300;
-    font-size: 16px;
-    margin-left: 10%;
-  }
-  .download img{
-    width: 50%;
-  }
+    .informations p {
 
-  .download h3{
-    color: #777;
-    font-weight: 300;
-    font-size: 16px;
-  }
-  </style>
-</body>
+      color: #303133;
+      font-size: 16px;
+      font-weight: 300;
+    }
+
+    .informations h2 {
+
+      color: #303133;
+      font-size: 16px;
+      font-weight: 300;
+    }
+
+    .informations a {
+      color: #303133;
+      text-decoration: none;
+      font-size: 16px;
+      font-weight: 300;
+    }
+
+    .informations img{
+      width: 25px;
+    }
+
+    .media {
+      display: flex;
+    }
+
+    .media a{
+      padding-right:7px;
+      padding-left: 7px;
+    }
+
+    /* --- Téléchargement Partfolio --- */
+    .download {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      align-content: center;
+      text-align: center;
+      color: #777;
+      font-weight: 300;
+      font-size: 16px;
+      margin-left: 10%;
+    }
+    .download img{
+      width: 50%;
+    }
+
+    .download h3{
+      color: #777;
+      font-weight: 300;
+      font-size: 16px;
+    }
+    </style>
+
+  </body>
 </html>
