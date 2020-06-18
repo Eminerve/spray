ed48695ac802ad5403b48cc6f85672733c5c6d1f
Eminerve <60135711+Eminerve@users.noreply.github.com>
2020-06-18 11:52:47 +0200

    add artists page content for a sample

--- artists.php
+++ artists.php
@@ -8,8 +8,33 @@
 </head>
 <body>
   <?php include('nav.php'); ?>
-  <section id="contact">
-
+  <section id="artists">
+    <div class="titre">
+        <h1>Notre équipe pour réussir tous vos projets et évènements !</h1>
+    </div>
+    <div class="container">
+      <div class="element">
+        <img class="personne1" src="images/medias/nabil_dorbane.jpg" width="250px" height="320px" alt="">
+        <div class="description personne1">
+          <h1>Nabil</h1>
+          <p>petite description avec son parcours, son univers, son utilité/poste dans l'entreprise ...</p>
+        </div>
+      </div>
+      <div class="element">
+        <img class="nabil" src="images/medias/nabil_dorbane.jpg" width="250px" height="320px" alt="">
+        <div class="description nabil">
+          <h1>Nabil</h1>
+          <p>petite description avec son parcours, son univers, son utilité/poste dans l'entreprise ...</p>
+        </div>
+      </div>
+      <div class="element">
+        <img class="personne2" src="images/medias/nabil_dorbane.jpg" width="250px" height="320px" alt="">
+        <div class="description personne2">
+          <h1>Nabil</h1>
+          <p>petite description avec son parcours, son univers, son utilité/poste dans l'entreprise ...</p>
+        </div>
+      </div>
+    </div>
   </section>
   <?php include('footer.php'); ?>
 
@@ -21,10 +46,106 @@
   </script>
 
   <style media="screen">
-    body {
-      opacity:0;
-      transition: 300ms opacity;
-    }
+
+/* ---------------- Fonts de la page --------------- */
+
+  @font-face {
+    font-family: 'OpenSans';
+    src: url('fonts/OpenSans-Light.ttf');
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'OpenSans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('fonts/OpenSans-Regular.ttf');
+  }
+
+/* --- Caractéristiques générales de la page ----- */
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 14px;
+    font-family: 'OpenSans', halvetica, sans-serif,serif;
+    color: #303133;
+  }
+
+  body {
+    animation: fadeIn linear .5s;
+    -webkit-animation: fadeIn linear .5s;
+    -moz-animation: fadeIn linear .5s;
+    -o-animation: fadeIn linear .5s;
+    -ms-animation: fadeIn linear .5s;
+  }
+
+/*--------------- container ---------------------- */
+
+  .container{
+    padding-top: 3rem;
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    margin: auto;
+    text-align: center;
+    justify-content: space-around;
+    align-items: flex-start;
+    align-content: space-between;
+  }
+
+  .element{
+    width: 250px;
+  }
+
+  .description{
+    display: none;
+  }
+
+  .description h1{
+    line-height: 50px;
+    font-weight: 400;
+  }
+  .description p{
+    padding: 4px;
+    font-weight: 300;
+    color: #777;
+  }
+
+  .titre{
+    padding-top: 5rem;
+  }
+
+  .titre h1{
+    font-size: 1.5rem;
+    font-weight: 400;
+    text-align: center;
+  }
   </style>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+
+<!-- Show on hover -->
+$(document).ready(function(){
+  $(".personne1").hover(function(){
+    $(".description.personne1").slideDown("slow");
+  });
+  $(".personne2").hover(function(){
+    $(".description.personne2").slideDown("slow");
+  });
+  $(".nabil").hover(function(){
+    $(".description.nabil").slideDown("slow");
+  });
+});
+</script>
 </body>
 </html>
